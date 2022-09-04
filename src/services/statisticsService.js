a369e04c4fc0d8b30b8914b3b9048f04a0fd2e09
glebremniov <g.remniov@gmail.com>
2022-09-04 21:20:32 +0300

    Fix bug under daily statistics

--- src/services/statisticsService.js
+++ src/services/statisticsService.js
@@ -1,11 +1,10 @@
 import _ from "lodash";
 
 const buildStatisticsData = (rawData) => {
-
-    const daysCount = rawData.length;
     const totalIncome = _.sum(rawData.flat().map(({ totalPriceAfterTaxes }) => totalPriceAfterTaxes));
     const operationsCount = rawData.flat().length;
-    const workedDays = rawData.flat().map(({ date }) => date);
+    const workedDays = _.uniq(rawData.flat().map(it => it.dateFormatted))
+    const daysCount = workedDays.length;
 
     return {
         generalData: {
