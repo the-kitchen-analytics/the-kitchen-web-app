bf343f512b1b642a7c9b82022c33cd437c720a3f
glebremniov <g.remniov@gmail.com>
2022-09-28 21:44:56 +0300

    Revert NoContentView

--- src/pages/NoContent/NoContent.jsx
+++ src/pages/NoContent/NoContent.jsx
@@ -1,30 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Container, Divider, Icon, Message, Segment } from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
 
 const NoContentView = () => (
-    <Container>
-        <Segment>
-            <Message
-                size="large"
-                warning
-            >
-                <Message.Header>
-                    <Icon name="exclamation circle" />
-                    Такой страницы не существует
-                </Message.Header>
-
-                <Divider />
-
-                <Message.Content>
-                    <p>
-                        Вы можете вернуться на {<Link to='/'>главную</Link>} страницу.
-                    </p>
-                </Message.Content>
-
-            </Message>
-        </Segment>
-    </Container>
+    <div className="view">
+        <Icon name="info circle" />
+        <span>Нет данных</span>
+    </div>
 );
 
 export default NoContentView;
