2612bf17672e4ffe3cf198531b853b86dc278c47
Gleb Remniov <g.remniov@gmail.com>
2023-01-24 22:14:49 +0200

    Update Firebase secrets config (#12)

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -26,9 +26,8 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_STORAGE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_STORAGE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MESUREMENT_ID
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
