08e69928a417547574176af91b2b9e03f953c212
Gleb Remniov <g.remniov@gmail.com>
2023-06-25 16:37:47 +0300

    Minor fix

--- src/utils/statistics/buildProgressData.js
+++ src/utils/statistics/buildProgressData.js
@@ -1,12 +1,32 @@
 import _ from 'lodash'
 
-const buildProgressData = (data) =>
-  Object.entries(_.groupBy(data, data => data.type))
-    .map(([key, value]) => ({
-      key,
-      value: value.length,
-      totalPriceBeforeTaxes: _.sumBy(value, item => item.priceBeforeTaxes),
-      totalPriceAfterTaxes: _.sumBy(value, item => item.priceAfterTaxes)
-    }))
+const getType = (entry) => {
+  if (entry.type) {
+    return entry.type
+  }
+
+  if (!_.isEmpty(entry.types)) {
+    return entry.types[0]
+  }
+
+  return 'Unknown'
+}
+
+const groupDataByType = (entries) => {
+  return _.groupBy(entries.map(entry => ({
+    ...entry,
+    type: getType(entry)
+  })), ({ type }) => type)
+}
+
+const mapEntry = ([key, value]) => ({
+  key,
+  value: value.length,
+  totalPriceBeforeTaxes: _.sumBy(value, item => item.priceBeforeTaxes),
+  totalPriceAfterTaxes: _.sumBy(value, item => item.priceAfterTaxes)
+})
+
+const buildProgressData = (data) => Object.entries(groupDataByType(data))
+  .map(mapEntry)
 
 export default buildProgressData
