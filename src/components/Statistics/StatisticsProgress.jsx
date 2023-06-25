f76c2cc2a9e572081944ec40da1ded2e772c2183
Gleb Remniov <g.remniov@gmail.com>
2023-06-25 21:25:57 +0300

    Feature/progress bar disable animation (#24)
    * Refine StatisticsProgress
    
    * Bump app version

--- src/components/Statistics/StatisticsProgress.jsx
+++ src/components/Statistics/StatisticsProgress.jsx
@@ -2,6 +2,7 @@ import { Progress, Segment } from 'semantic-ui-react'
 import { sumBy } from 'lodash'
 import { getProcedureTypeDisplayName } from '../../utils/procedures'
 import { buildPriceString } from '../../utils/money'
+import useColorNames from '../../hooks/useColorNames'
 
 const isEligibleItem = ({ value }) => value > 0
 
@@ -10,12 +11,15 @@ const StatisticsProgress = ({ data }) => {
   const filteredData = data.filter(isEligibleItem)
   const total = sumBy(filteredData, item => item.value)
 
+  const { getRandomFancyColorName } = useColorNames()
+
   return (
     <Segment>
       {
         filteredData.map(({ key, value, totalPriceAfterTaxes }) => (
           <Progress
-            indicating
+            color={getRandomFancyColorName()}
+            size="large"
             progress="value"
             key={key}
             label={buildPriceString(getProcedureTypeDisplayName(key), totalPriceAfterTaxes)}
