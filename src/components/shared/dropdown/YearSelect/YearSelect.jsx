25fa04b8e4cc8d9742c1990564764e55196e09b7
Gleb Remniov <g.remniov@gmail.com>
2024-01-08 22:35:27 +0200

    Migrate to Vite

--- src/components/shared/dropdown/YearSelect/YearSelect.jsx
+++ src/components/shared/dropdown/YearSelect/YearSelect.jsx
@@ -1,7 +1,6 @@
-
 import { Dropdown } from 'semantic-ui-react'
 
-export const YearSelect = ({ value, handleChange, options, disabled }) =>  (
+export const YearSelect = ({ value, handleChange, options, disabled }) => (
   <Dropdown
     disabled={disabled || options.length <= 1}
     placeholder='Выберите год'
