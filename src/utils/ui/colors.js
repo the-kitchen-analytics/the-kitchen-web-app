4bb4f463370e947a9af8c1f269c7d6a8b41667f9
Gleb Remniov <g.remniov@gmail.com>
2024-01-23 14:46:51 +0200

    Rename fancy to colorful

--- src/utils/ui/colors.js
+++ src/utils/ui/colors.js
@@ -2,7 +2,7 @@ import colors from '../../data/ui/colors.json'
 import { getRandomArrayElement } from '../'
 
 export const colorNames = colors.map(({ name }) => name)
-export const fancyColorNames = colors.filter(({ isFancy }) => isFancy)
+export const fancyColorNames = colors.filter(({ isColorful }) => isColorful)
   .map(({ name }) => name)
 
 export const getRandomColorName = () => getRandomArrayElement(colorNames)
