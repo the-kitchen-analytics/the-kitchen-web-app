b1eb7172e5e9a21cf5c9814e11bb86f8cce4aa20
Gleb Remniov <g.remniov@gmail.com>
2024-01-23 14:25:55 +0200

    Add smooth scroll on route change

--- src/components/layouts/MainLayout/MainLayout.jsx
+++ src/components/layouts/MainLayout/MainLayout.jsx
@@ -1,15 +1,18 @@
+import { useLayoutEffect } from 'react'
+import { Outlet, useLocation, useOutletContext } from 'react-router-dom'
 import { Grid } from 'semantic-ui-react'
-import { Outlet, useOutletContext } from 'react-router-dom'
-import {
-  ReceiptContextProvider,
-  UserDetailsContextProvider,
-  UserSettingsContextProvider
-} from '../../../context'
+import { ReceiptContextProvider, UserDetailsContextProvider, UserSettingsContextProvider } from '../../../context'
 import { NavigationMenu } from '../../NavigationMenu'
 import './MainLayout.css'
 
 export const MainLayout = () => {
   const { user } = useOutletContext()
+
+  const location = useLocation()
+  // Scroll to top if path changes
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [location.pathname])
 
   return (
     <UserSettingsContextProvider>
