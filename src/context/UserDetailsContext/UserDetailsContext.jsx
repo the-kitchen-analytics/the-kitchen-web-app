c4d9123959905e85a567b664e914b890f5c8fdca
Gleb Remniov <g.remniov@gmail.com>
2024-01-23 14:27:24 +0200

    Code style fix

--- src/context/UserDetailsContext/UserDetailsContext.jsx
+++ src/context/UserDetailsContext/UserDetailsContext.jsx
@@ -4,15 +4,15 @@ import { useUserDetails } from './useUserDetails'
 
 export const UserDetailsContext = createContext({})
 export const UserDetailsContextProvider = ({ uid, children }) => {
-  
+
   const { userDetails, updateUserDetails, isLoading, error } = useUserDetails(uid)
-  
+
   if (isLoading) {
     return <Loader content={'Загрузка настроек пользователя'} />
   }
 
   return (
-    <UserDetailsContext.Provider value={[ userDetails, updateUserDetails, isLoading, error ]}>
+    <UserDetailsContext.Provider value={[userDetails, updateUserDetails, isLoading, error]}>
       {
         children
       }
