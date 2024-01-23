1e488a8b594029bfd76efbc853f6006e02204e3a
Gleb Remniov <g.remniov@gmail.com>
2024-01-23 20:12:23 +0200

    Remove comment

--- src/shared/hooks/useProcedures.js
+++ src/shared/hooks/useProcedures.js
@@ -9,31 +9,3 @@ export const useProcedures = (workerCategory) => {
 
   return [data, isLoading, hasError]
 }
-
-// export const useProcedures = (options) => {
-//   const { workerCategory } = options
-//
-//   const [data, setData] = useState()
-//   const [error, setError] = useState()
-//   const [isLoading, setIsLoading] = useState(true)
-//
-//   useEffect(() => {
-//     return subscribe(workerCategory,
-//       (querySnapshot) => {
-//         setIsLoading(true)
-//
-//         const data = getDocsData(querySnapshot)
-//         console.debug('procedures', workerCategory, data)
-//
-//         setData(data)
-//         setIsLoading(false)
-//       },
-//       error => {
-//         setError(error)
-//         setIsLoading(false)
-//       }
-//     )
-//   }, [workerCategory])
-//
-//   return [data, isLoading, error]
-// }
