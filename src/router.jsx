12baa1a6fa6388083ce3386915dde0c0378201f6
Gleb Remniov <g.remniov@gmail.com>
2024-01-23 15:24:14 +0200

    Move router to a separate file

---
+++ src/router.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter, Navigate } from 'react-router-dom'
+
+import {
+  CreateAccountPage, LoginPage, ResetPasswordPage,
+  EditReceiptPage, CreateReceiptPage,
+  SettingsPage, InfoPage,
+  TablePage, StatisticsPage
+} from './pages'
+
+import {
+  RECEIPT_CREATE_PATH, RECEIPT_EDIT_PATH,
+  INFO_PATH, LOGIN_PATH, REGISTER_PATH, RESET_PASSWORD_PATH,
+  SETTINGS_PATH, STATISTICS_PATH, TABLE_PATH
+} from './data/routePaths'
+
+import { MainLayout, RequireAuth } from './layouts'
+
+export const router = createBrowserRouter([
+  { index: true, element: <Navigate to={TABLE_PATH} /> },
+  { path: LOGIN_PATH, element: <LoginPage /> },
+  { path: REGISTER_PATH, element: <CreateAccountPage /> },
+  { path: RESET_PASSWORD_PATH, element: <ResetPasswordPage /> },
+  {
+    element: <RequireAuth />, children: [
+      {
+        element: <MainLayout />, children: [
+          { path: TABLE_PATH, element: <TablePage /> },
+          { path: STATISTICS_PATH, element: <StatisticsPage /> },
+          { path: RECEIPT_EDIT_PATH, element: <EditReceiptPage /> },
+          { path: RECEIPT_CREATE_PATH, element: <CreateReceiptPage /> },
+          { path: SETTINGS_PATH, element: <SettingsPage /> },
+          { path: INFO_PATH, element: <InfoPage /> }
+        ]
+      }
+    ]
+  },
+  { path: '*', element: <Navigate to={TABLE_PATH} /> }
+])
