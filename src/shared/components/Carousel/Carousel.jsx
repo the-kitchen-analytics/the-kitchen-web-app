5a93d407f10b9ee14609028852afb3f57f52355f
Gleb Remniov <g.remniov@gmail.com>
2024-02-04 17:15:47 +0200

    Fix button sizes

--- src/shared/components/Carousel/Carousel.jsx
+++ src/shared/components/Carousel/Carousel.jsx
@@ -1,20 +1,22 @@
 import { Button } from 'semantic-ui-react'
 import { useTheme } from '../../hooks'
+import './Carousel.css'
 
 export const Carousel = ({ leftButton, resetButton, rightButton }) => {
 
   const { size } = useTheme()
 
   const buttons = [
-    { key: 'left', icon: 'chevron left', ...leftButton },
+    { key: 'left', icon: 'chevron left', className: 'left-button', ...leftButton },
     { key: 'reset', content: 'По умолчанию', ...resetButton },
-    { key: 'right', icon: 'chevron right', ...rightButton }
+    { key: 'right', icon: 'chevron right', className: 'right-button', ...rightButton }
   ]
 
   return (
     <Button.Group
       fluid
       basic
+      className={'carousel'}
       size={size}
       buttons={buttons}
     />
--- src/shared/components/Dropdown/DaySelect/DaySelect.jsx
+++ src/shared/components/Dropdown/DaySelect/DaySelect.jsx
@@ -4,7 +4,9 @@ export const DaySelect = ({ value, handleChange, options = [], disabled }) => (
   <DefaultSelect
     disabled={disabled || options.length <= 1}
     placeholder={'Выберите день'}
-    search selection
+    basic
+    button
+    search
     options={options}
     onChange={handleChange}
     value={value}
