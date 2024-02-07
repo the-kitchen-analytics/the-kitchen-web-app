d0bcf45ae622a296aab16f911737b8031aad8aa8
Gleb Remniov <g.remniov@gmail.com>
2024-02-07 16:06:29 +0200

    Minor fix NavigationMenu

--- src/layouts/MainLayout/NavigationMenu/NavigationMenu.jsx
+++ src/layouts/MainLayout/NavigationMenu/NavigationMenu.jsx
@@ -1,36 +1,7 @@
 import { Menu } from 'semantic-ui-react'
-import { RECEIPT_CREATE_PATH, INFO_PATH, SETTINGS_PATH, STATISTICS_PATH, TABLE_PATH } from '../../../router'
-import { NavigationMenuItem } from './NavigationMenuItem'
 import { useTheme } from '../../../shared/hooks'
+import { menuItems } from './helpers'
 import './NavigationMenu.css'
-
-const menuItems = [
-  {
-    name: 'home',
-    icon: 'home',
-    to: TABLE_PATH
-  },
-  {
-    name: 'statistics',
-    icon: 'chart pie',
-    to: STATISTICS_PATH
-  },
-  {
-    name: 'create-receipt',
-    icon: 'plus circle',
-    to: RECEIPT_CREATE_PATH
-  },
-  {
-    name: 'info',
-    icon: 'info circle',
-    to: INFO_PATH
-  },
-  {
-    name: 'settings',
-    icon: 'setting',
-    to: SETTINGS_PATH
-  }
-]
 
 export const NavigationMenu = () => {
   const theme = useTheme()
@@ -38,15 +9,11 @@ export const NavigationMenu = () => {
   return (
     <Menu
       {...theme}
-      className={'navigation-menu'}
-      icon
       borderless
+      className={'navigation-menu'}
       fixed={'bottom'}
       widths={menuItems.length}
-    >
-      {
-        menuItems.map(item => <NavigationMenuItem key={item.name} {...item} />)
-      }
-    </Menu>
+      items={menuItems}
+    />
   )
 }
---
+++ src/layouts/MainLayout/NavigationMenu/helpers.jsx
@@ -0,0 +1,41 @@
+import { NavLink } from 'react-router-dom'
+import { INFO_PATH, RECEIPT_CREATE_PATH, SETTINGS_PATH, STATISTICS_PATH, TABLE_PATH } from '../../../router'
+
+const getIconProps = (name) => ({
+  link: true,
+  size: 'big',
+  name
+})
+
+export const menuItems = [
+  {
+    key: 'home',
+    icon: 'home',
+    to: TABLE_PATH,
+  },
+  {
+    key: 'statistics',
+    icon: 'chart pie',
+    to: STATISTICS_PATH,
+  },
+  {
+    key: 'create-receipt',
+    icon: 'plus circle',
+    to: RECEIPT_CREATE_PATH,
+  },
+  {
+    key: 'info',
+    icon: 'info circle',
+    to: INFO_PATH,
+  },
+  {
+    key: 'settings',
+    icon: 'setting',
+    to: SETTINGS_PATH,
+  }
+].map(({ icon, ...item }) => ({
+  ...item,
+  as: NavLink,
+  icon: getIconProps(icon),
+  className: 'navigation-menu-item'
+}))
