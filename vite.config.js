1dc3d25ccf47c167d212ebedacc305792bedb9d8
Hleb Ramniou <g.remniov@gmail.com>
2024-02-11 17:26:49 +0200

    Disable vite-plugin-pwa (#105)

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import eslint from 'vite-plugin-eslint'
-import { VitePWA } from 'vite-plugin-pwa'
+//import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     eslint(),
-    VitePWA({ registerType: 'autoUpdate' })
+    //    VitePWA({ registerType: 'autoUpdate' })
   ],
 })
